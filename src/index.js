d866fc1e3611df6a4a8265534a1c6873598eeea5
Megan Horsfield <meganthadev@gmail.com>
2020-07-27 05:37:24 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+   loadImages();
+   loadBreeds();
+})
+
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+  return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(imgUrl) {
+  const container = document.getElementById("dog-image-container");
+  const newImg = document.createElement('img');
+  newImg.src = imgUrl;
+  container.appendChild(newImg);
+}
+
+function loadBreeds() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+  return fetch(brdUrl)
+    .then(resp => resp.json())
+    
